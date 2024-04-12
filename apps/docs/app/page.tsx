ca01fe195df2d194502b64ed364ddb5d2aca5fe9
moksh <moxvankar2005@gmail.com>
2024-04-12 19:52:35 +0530

    init

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@ignotus/ui/card";
+import { Code } from "@ignotus/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@ignotus/ui/button";
 
 function Gradient({
   conic,
