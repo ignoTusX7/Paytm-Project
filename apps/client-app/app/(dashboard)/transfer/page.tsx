284697cf7f700995367934bf8fad511915f8421f
moksh <moxvankar2005@gmail.com>
2024-04-13 15:58:43 +0530

    added transfer

---
+++ apps/client-app/app/(dashboard)/transfer/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { getBalance } from "../../lib/actions/getBalance";
+import { BalanceCard } from "../../../components/BalanceCard";
+import { AddMoney } from "../../../components/AddMoneyCard";
+import { TextInput } from "@ignotus/ui";
+
+export default async function Transfer() {
+  const balance = await getBalance();
+  return (
+    <div className="w-screen">
+      <div className="text-4xl text-[#00baf2] pt-8 mb-8 font-bold">
+        Transfer
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+        <div><AddMoney /></div>
+        <div>
+          <BalanceCard amount={balance.amount} locked={balance.locked} />
+          <div className="pt-4">
+            {/* <OnRampTransactions transactions={transactions} /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
