bcc2a365e1b16600746f9271311079048a1faea6
moksh <moxvankar2005@gmail.com>
2024-04-13 11:47:43 +0530

    make provider client component

--- apps/client-app/app/providers.tsx
+++ apps/client-app/app/providers.tsx
@@ -1,6 +1,12 @@
+"use client"
+import { SessionProvider } from "next-auth/react";
 import React from "react";
 import { RecoilRoot } from "recoil";
 
-export const provider = ({ children }: { children: React.ReactNode }) => {
-  return <RecoilRoot>{children}</RecoilRoot>;
-};
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <RecoilRoot>
+      <SessionProvider>{children}</SessionProvider>
+    </RecoilRoot>
+  );
+}
