baad987239dbf746bc301848b57d68b6d7a9ad6a
moksh <moxvankar2005@gmail.com>
2024-04-13 19:37:41 +0530

    added OnRamp Section

--- apps/client-app/app/(dashboard)/transfer/page.tsx
+++ apps/client-app/app/(dashboard)/transfer/page.tsx
@@ -2,21 +2,26 @@ import React from "react";
 import { getBalance } from "../../lib/actions/getBalance";
 import { BalanceCard } from "../../../components/BalanceCard";
 import { AddMoney } from "../../../components/AddMoneyCard";
-import { TextInput } from "@ignotus/ui";
+import { OnRampTransactions } from "../../../components/OnRampTransactions";
+import { getOnRampTransactions } from "../../lib/actions/getOnRampTransactions";
 
 export default async function Transfer() {
   const balance = await getBalance();
+  const transactions = await getOnRampTransactions();
+
   return (
     <div className="w-screen">
       <div className="text-4xl text-[#00baf2] pt-8 mb-8 font-bold">
         Transfer
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
-        <div><AddMoney /></div>
+        <div>
+          <AddMoney />
+        </div>
         <div>
           <BalanceCard amount={balance.amount} locked={balance.locked} />
           <div className="pt-4">
-            {/* <OnRampTransactions transactions={transactions} /> */}
+            <OnRampTransactions transactions={transactions} />
           </div>
         </div>
       </div>
