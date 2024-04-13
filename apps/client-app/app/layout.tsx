d1e0ae98e1a10b799e169071b46f8a937f6771aa
moksh <moxvankar2005@gmail.com>
2024-04-13 18:39:45 +0530

    added Header

--- apps/client-app/app/layout.tsx
+++ apps/client-app/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "./providers";
+import { Header } from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-        {children}
+          <Header />
+          {children}
         </Providers>
-        </body>
+      </body>
     </html>
   );
 }
