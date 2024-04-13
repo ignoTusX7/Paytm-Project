7d25e5a62dee1b46521e1645dc09779c0a148dc6
moksh <moxvankar2005@gmail.com>
2024-04-13 09:40:37 +0530

    added prisma package

---
+++ packages/db/src/seed.ts
@@ -0,0 +1,52 @@
+import db from "./index";
+import bcrypt from "bcrypt";
+
+async function main() {
+  const bob = await db.user.upsert({
+    where: {
+      mobileNumber: "9876543210",
+    },
+    create: {
+      name: "Bob",
+      mobileNumber: "9876543210",
+      email: "bob@example.com",
+      password: await bcrypt.hash("123456", 10),
+      Balance: {
+        create: {
+          amount: 100000,
+          locked: 0,
+        },
+      },
+    },
+    update: {},
+  });
+  const alice = await db.user.upsert({
+    where: {
+      mobileNumber: "1234567890",
+    },
+    create: {
+      name: "Bob",
+      mobileNumber: "1234567890",
+      email: "alice@example.com",
+      password: await bcrypt.hash("123456", 10),
+      Balance: {
+        create: {
+          amount: 100000,
+          locked: 0,
+        },
+      },
+    },
+    update: {},
+  });
+  console.log({ bob, alice });
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
