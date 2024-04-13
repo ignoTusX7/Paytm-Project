591a70e43efb17521a66b464c19b09ebf260d961
moksh <moxvankar2005@gmail.com>
2024-04-13 15:59:32 +0530

    fixed side bar

--- apps/client-app/app/(dashboard)/layout.tsx
+++ apps/client-app/app/(dashboard)/layout.tsx
@@ -1,30 +1,22 @@
-import { SidebarItem } from "../../components/SidebarItems";
-import { HomeIcon } from "../../components/svg/HomeIcon";
-import { TransactionIcon } from "../../components/svg/TransactionIcon";
-import { TransferIcon } from "../../components/svg/TransferIcon";
-import {PtoPIcon} from '../../components/svg/ptopIcon'
+import { getServerSession } from "next-auth";
+import { Sidebar } from "../../components/Sidebar";
+import { authOption } from "../lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Layout({
+export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
-}): JSX.Element {
+}): Promise<JSX.Element> {
+
+  const session = await getServerSession(authOption);
+  if (!session?.user) {
+    return redirect("/api/auth/signin");
+  }
   return (
     <div className="flex">
-        <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-28">
-            <div>
-                <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
-                <SidebarItem href={"/transfer"} icon={<TransferIcon />} title="Transfer" />
-                <SidebarItem href={"/transactions"} icon={<TransactionIcon />} title="Transactions" />
-                <SidebarItem href={"/p2p"} icon={<PtoPIcon/>} title="P2P Transfer" />
-            </div>
-        </div>
-            {children}
+      <Sidebar />
+      {children}
     </div>
   );
 }
-
-// Icons Fetched from https://heroicons.com/
-
-
-
