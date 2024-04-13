65cad7dbff089dc4e503d73d7a018f39157763fb
moksh <moxvankar2005@gmail.com>
2024-04-13 11:41:45 +0530

    added recoil

---
+++ apps/merchant-app/app/provider.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { RecoilRoot } from "recoil"
+
+export const Providers = ({children}: {children : React.ReactNode})=>{
+    return (
+        <RecoilRoot>
+
+            {children}
+        </RecoilRoot>
+    )
+}
