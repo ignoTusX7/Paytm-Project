b476add91fe797c91ea971ebe8219da331fcae50
moksh <moxvankar2005@gmail.com>
2024-04-14 10:51:08 +0530

    added p2p

--- apps/client-app/app/(dashboard)/p2p/page.tsx
+++ apps/client-app/app/(dashboard)/p2p/page.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { SendCard } from "../../../components/SendCard";
 
 export default function P2P() {
-  return <div>
-    <SendCard/>
-  </div>;
+  return (
+    <div>
+      <SendCard />
+    </div>
+  );
 }
