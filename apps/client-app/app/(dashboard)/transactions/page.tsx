5bfa00f7b59015e34a534fbe5f841e7ceb0a9b69
moksh <moxvankar2005@gmail.com>
2024-04-14 18:58:03 +0530

    Added Transactions

--- apps/client-app/app/(dashboard)/transactions/page.tsx
+++ apps/client-app/app/(dashboard)/transactions/page.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { getP2PTransfers } from "../../lib/actions/getp2pTransfers";
+import { OnRampTransactions } from "../../../components/OnRampTransactions";
+import { getOnRampTransactions } from "../../lib/actions/getOnRampTransactions";
+import { P2PTransactions } from "../../../components/p2pTransactions";
 
-export default function Transactions() {
+export default async function Transactions() {
+  const p2pTransactions = await getP2PTransfers();
+  const OnRampTransactionsData = await getOnRampTransactions();
+
   return (
-    <div>Transactions</div>
-  )
+    <div className="w-screen p-4">
+      <div className="p-4">
+        <P2PTransactions transactions={p2pTransactions} />
+        {/* <div>
+          <div className="">
+            <OnRampTransactions transactions={OnRampTransactionsData} />
+          </div>
+        </div> */}
+      </div>
+    </div>
+  );
 }
