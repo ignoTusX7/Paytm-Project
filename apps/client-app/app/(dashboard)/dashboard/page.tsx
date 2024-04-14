a467a4c02499c0b635b36d40e4c6ca4bcb2c3bdd
moksh <moxvankar2005@gmail.com>
2024-04-14 18:57:46 +0530

    fixes

--- apps/client-app/app/(dashboard)/dashboard/page.tsx
+++ apps/client-app/app/(dashboard)/dashboard/page.tsx
@@ -3,13 +3,12 @@ import React from "react";
 import { INewSession, authOption } from "../../lib/auth";
 import { Card } from "@ignotus/ui";
 import { getBalance } from "../../lib/actions/getBalance";
-import { BalanceCard } from "../../../components/BalanceCard";
 
 export default async function Dashboard() {
   const session: INewSession | null = await getServerSession(authOption);
   const balance = await getBalance();
   return (
-    <div className="w-screen">
+    <div className="w-[90%] mx-auto md:w-screen">
       <div className="text-3xl fon-semibold mt-4 flex gap-2">
         <h3 className="font-bold text-[#00baf2]">Hii, </h3>
         {session?.user.name}.
@@ -19,15 +18,15 @@ export default async function Dashboard() {
           <Card title={"Wallet"}>
             <div className="flex justify-between border-b border-slate-300 pb-2">
               <div>Unlocked balance</div>
-              <div>{balance.amount / 100} INR</div>
+              <div>{(balance.amount / 100).toLocaleString("en-IN", { style: "currency", currency: "INR" })}</div>
             </div>
             <div className="flex justify-between border-b border-slate-300 py-2">
               <div>Total Locked Balance</div>
-              <div>{balance.locked / 100} INR</div>
+              <div>{(balance.locked / 100).toLocaleString("en-IN", { style: "currency", currency: "INR" })}</div>
             </div>
             <div className="flex justify-between border-b border-slate-300 py-2">
               <div>Total Balance</div>
-              <div>{(balance.locked + balance.amount) / 100} INR</div>
+              <div>{((balance.locked + balance.amount) / 100).toLocaleString("en-IN", { style: "currency", currency: "INR" })}</div>
             </div>
           </Card>
         </div>
--- apps/client-app/app/(dashboard)/p2p/page.tsx
+++ apps/client-app/app/(dashboard)/p2p/page.tsx
@@ -3,7 +3,7 @@ import { SendCard } from "../../../components/SendCard";
 
 export default function P2P() {
   return (
-    <div>
+    <div className="mx-auto md:mx-0">
       <SendCard />
     </div>
   );
