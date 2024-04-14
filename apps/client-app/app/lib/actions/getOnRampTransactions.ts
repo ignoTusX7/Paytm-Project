d1596a00aad2e2c6e546badce34ce1d52c51ca00
moksh <moxvankar2005@gmail.com>
2024-04-14 18:53:28 +0530

    fixed onramp

--- apps/client-app/app/lib/actions/getOnRampTransactions.ts
+++ apps/client-app/app/lib/actions/getOnRampTransactions.ts
@@ -1,22 +1,22 @@
 "use server";
 import { getServerSession } from "next-auth";
 import { authOption } from "../auth";
-import db from "@ignotus/db/client"
+import db from "@ignotus/db/client";
 
 export async function getOnRampTransactions() {
-    const session = await getServerSession(authOption);
-    const txns = await db.onRampTransaction.findMany({
-        where: {
-            userId: Number(session?.user?.id)
-        },
-        orderBy : {
-            startTime: "desc"
-        }
-    });
-    return txns.map(t => ({
-        time: t.startTime,
-        amount: t.amount,
-        status: t.status,
-        provider: t.provider
-    }))
+  const session = await getServerSession(authOption);
+  const txns = await db.onRampTransaction.findMany({
+    where: {
+      userId: Number(session?.user?.id),
+    },
+    orderBy: {
+      id: "desc",
+    },
+  });
+  return txns.map((t) => ({
+    time: t.startTime,
+    amount: t.amount,
+    status: t.status,
+    provider: t.provider,
+  }));
 }
